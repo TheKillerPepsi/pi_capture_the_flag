2e04be7e6c2c7435cf28be0489aaff19fe2de955
Pepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-10-02 22:03:10 +0200

    Create first_skript.py

---
+++ playground/first_skript.py
@@ -0,0 +1,24 @@
+#sourche: https://tutorials-raspberrypi.de/raspberry-pi-gpio-erklaerung-beginner-programmierung-lernen/
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.IN)
+
+for i in range(5):
+    GPIO.output(23, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(23, GPIO.LOW)
+    time.sleep(0.5)
+
+# Endlosschleife
+while True:
+    if GPIO.input(24) == 0:
+        # Ausschalten
+        GPIO.output(23, GPIO.LOW)
+    else:
+        # Einschalten
+        GPIO.output(23, GPIO.HIGH)
